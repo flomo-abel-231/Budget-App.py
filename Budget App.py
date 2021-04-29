7dc644b97107d59aab3a6740d33cedc949e5b195
flo-231 <flomoabelt@gmail.com>
2021-04-29 17:40:47 +0000

    Update Budget App.py

--- Budget App.py
+++ Budget App.py
@@ -1,5 +1,5 @@
 
-class Budget :
+class Category :
     
 
     def __init__ (self,category,amount):
@@ -10,52 +10,36 @@ class Budget :
                
       #methods
     def deposit(self):
-        import datetime
-        today = datetime.datetime.now()
-        print("Welcome to the deposit method")
-        food = 50000
-        clothing = 250000
-        car_expenses = 756000
-        print(today)
-        print (" Total deposit is",food + clothing + car_expenses )
+        self.amount += self.amount
+        return "You have successfully deposited: {} in {} category".format(self.amount,self.category)
 
-        pass
+    def check_balance ():
+        #this should return a true or false, it checks if amount is less or greater than self.amount
+        return False 
 
-    def check_balance(self):
-        import datetime
-        today = datetime.datetime.now()
-        print ( 'Welcome to the the check balance entry' )
-        print(today)
-        pass
+    def budget_balance(self):
 
-    def withdrawal (self):
-        import datetime
-        today = datetime.datetime.now()
-        print(today)
-        print ('Welcome to the withdrawal section')
-        pass
-
-    def transfer (self):
-        import datetime
-        today = datetime.datetime.now()
-        print(today)
-        print ('Welcome to the transfer section')
-        pass
-        
-
-
-
-
-        
-budget1 = Budget("food", 50000)
-
-budget2 = Budget ("clothing", 250000)
-
-budget3 = Budget("car_expenses", 756000)
+        return "your current balance is: {}".format(self.amount)
     
-print(budget1.deposit())    
+    
+    def withdrawal (self):
+       # reverse of deposit
+        self.amount - self.amount 
+        return " you have successfully withdrew {} in {} category".format(self.amount, self.category)
 
+    def transfer (self,amount,deposit):
+        #transfer between two instantiated categories
+        return self.withdrawal (amount) + " " + self.deposit(amount)
+        
 
+food_category = Category("Food", 1000)
+clothing_category = Category("Clothing", 20)
+car_expenses_category = Category("Car expenses", 75000)
+appliances_category = Category("Appliances", 6000)
 
-
-
+print(food_category.deposit())
+print(food_category.withdrawal())
+print(food_category.budget_balance())
+#print(food_category.transfer(5000,appliances_category)) 
+#print(food_category.check_balance( 30000000))
+        
