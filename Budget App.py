c8a9ff14fb33ea2f404d3466acbff37d028d390b
flo-231 <flomoabelt@gmail.com>
2021-04-29 13:05:49 +0000

    Update Budget App.py

--- Budget App.py
+++ Budget App.py
@@ -1,40 +1,59 @@
+
 class Budget :
-    def __init__ (self):
-        pass
-        
-        
-        
-budget1 = Budget()
-budget1.food = {
-    'Rice':'1500usd',
-    'Beans':'1000usd',
-    'Fish':'2000usd',
-    'Meat': '3000usd',
-    'Greens': '4000usd'
-    }
-
-budget2 = Budget ()
-budget2.clothing = {
-    'Shirts':'4000usd',
-    'Trousers': '2000usd',
-    'Boxers': '2000usd',
-    'Sneakers': '3000usd'
-    }    
-
-budget3 = Budget()
-budget3.car_expenses = {
-    'Engine oil': '2000usd',
-    'Gasoline': '3000usd',
-    'Shock absorber': '3000usd',
-    'Tyres': '4000usd',
-    'Car wash': '2000usd',
-    'Other services': '5000usd' 
-    }
     
 
-print(budget1.food["Rice"])
-print(budget2.clothing["Trousers"])
-print(budget3.car_expenses["Other services"])
+    def __init__ (self,category,amount):
+        self.category = category
+        self.amount = amount 
+        pass 
+
+               
+      #methods
+    def deposit(self):
+        import datetime
+        today = datetime.datetime.now()
+        print("Welcome to the deposit method")
+        food = 50000
+        clothing = 250000
+        car_expenses = 756000
+        print(today)
+        print (" Total deposit is",food + clothing + car_expenses )
+
+        pass
+
+    def check_balance(self):
+        import datetime
+        today = datetime.datetime.now()
+        print ( 'Welcome to the the check balance entry' )
+        print(today)
+        pass
+
+    def withdrawal (self):
+        import datetime
+        today = datetime.datetime.now()
+        print(today)
+        print ('Welcome to the withdrawal section')
+        pass
+
+    def transfer (self):
+        import datetime
+        today = datetime.datetime.now()
+        print(today)
+        print ('Welcome to the transfer section')
+        pass
+        
+
+
+
+
+        
+budget1 = Budget("food", 50000)
+
+budget2 = Budget ("clothing", 250000)
+
+budget3 = Budget("car_expenses", 756000)
+    
+print(budget1.deposit())    
 
 
 
